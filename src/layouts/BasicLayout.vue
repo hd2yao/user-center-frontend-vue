1892820b696494c7695d8f08d81e8a6840341654
hd2yao <1054290355@qq.com>
2024-12-13 17:10:38 +0800

    feat: 页脚布局

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="basic-layout">
+  <div id="basicLayout">
     <a-layout>
       <a-layout-header>Header</a-layout-header>
       <a-layout-content>Content</a-layout-content>
-      <a-layout-footer>Footer</a-layout-footer>
+      <a-layout-footer class="footer">
+        <a href="https://codefather.cn" target="_blank"> hd2yao by 海东 </a>
+      </a-layout-footer>
     </a-layout>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+#basicLayout .footer {
+  background: #efefef;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 16pc;
+}
+</style>
